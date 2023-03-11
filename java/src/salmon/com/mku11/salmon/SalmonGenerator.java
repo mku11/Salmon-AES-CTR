3315246e48f9280fe532cb8626b838956999db0e
mku11 <mku11@example.com>
2023-03-11 16:15:46 +0200

    version 2 of the generator

--- java/src/salmon/com/mku11/salmon/SalmonGenerator.java
+++ java/src/salmon/com/mku11/salmon/SalmonGenerator.java
@@ -35,32 +35,63 @@ import java.security.SecureRandom;
  * Utility class to be used with generating secure keys and initial vectors
  */
 public class SalmonGenerator {
-    private static final byte VERSION = 1;
-    private static final int MAGIC_LENGTH = 3;
-    private static final int VERSION_LENGTH = 1;
-    private static final int ITERATIONS_LENGTH = 4;
-    // iterations for the text derived master key
-    private static final int ITERATIONS = 65536;
+	private static final String MAGIC_BYTES = "SLM";
+    public static final byte VERSION = 2;
+    public static final int MAGIC_LENGTH = 3;
+    public static final int VERSION_LENGTH = 1;
+    public static final int ITERATIONS_LENGTH = 4;
+
+
     // should be 16 for AES256 the same as the iv
-    private static final int BLOCK_SIZE = 16;
+    public static final int BLOCK_SIZE = 16;
     // length for IV that will be used for encryption and master encryption of the combined key
-    private static final int IV_LENGTH = 16;
+    public static final int IV_LENGTH = 16;
     // encryption key length for AES256
-    private static final int KEY_LENGTH = 32;
+    public static final int KEY_LENGTH = 32;
     // encryption key length for HMAC256
-    private static final int HMAC_KEY_LENGTH = 32;
+    public static final int HMAC_KEY_LENGTH = 32;
     // result of the SHA256 should always be 256 bits
-    private static final int HMAC_RESULT_LENGTH = 32;
+    public static final int HMAC_RESULT_LENGTH = 32;
     // combined key is encryption key + HMAC key
-    private static final int COMBINED_KEY_LENGTH = KEY_LENGTH + HMAC_KEY_LENGTH;
+    public static final int COMBINED_KEY_LENGTH = KEY_LENGTH + HMAC_KEY_LENGTH;
     // master key to encrypt the combined key we also use AES256
-    private static final int MASTER_KEY_LENGTH = 32;
+    public static final int MASTER_KEY_LENGTH = 32;
     // salt size
-    private static final int SALT_LENGTH = 24;
+    public static final int SALT_LENGTH = 24;
     // vault nonce size
-    private static final int NONCE_LENGTH = 8;
-    private static final String MAGIC_BYTES = "SAL";
+    public static final int NONCE_LENGTH = 8;
+
+    // drive ID size
+    public static final int DRIVE_ID_LENGTH = 16;
+
+    // auth ID size
+    public static final int AUTH_ID_SIZE = 16;
+
+    public static final int CHUNKSIZE_LENGTH = 4;
+
     private static PbkdfType pbkdfType = PbkdfType.Default;
+
+
+    // iterations for the text derived master key
+    private static int iterations = 65536;
+
+    public static byte[] generateDriveID() {
+        return getSecureRandomBytes(DRIVE_ID_LENGTH);
+    }
+
+    public static byte[] getDefaultVaultNonce() {
+        byte[] bytes = new byte[NONCE_LENGTH];
+        return bytes;
+    }
+
+    public static byte[] getDefaultMaxVaultNonce() {
+        return BitConverter.toBytes(Long.MAX_VALUE, 8);
+    }
+
+    public static byte[] generateAuthId() {
+        return getSecureRandomBytes(AUTH_ID_SIZE);
+    }
+
 
     public enum PbkdfType {
         Default
@@ -71,24 +102,7 @@ public class SalmonGenerator {
     }
 
     /**
-     * Returns the byte length for the salt that will be used for encrypting the combined key (encryption key + HMAC key)
-     
-     */
-    public static int getSaltLength() {
-        return SALT_LENGTH;
-    }
-
-    /**
-     * Returns the byte length for the combined key (encryption key + HMAC key)
-     
-     */
-    public static int getCombinedKeyLength() {
-        return COMBINED_KEY_LENGTH;
-    }
-
-    /**
      * Gets the fixed magic bytes array
-     
      */
     public static byte[] getMagicBytes() {
         return MAGIC_BYTES.getBytes(Charset.defaultCharset());
@@ -96,72 +110,25 @@ public class SalmonGenerator {
 
     /**
      * Returns the Salmon format version
-     
      */
     public static byte getVersion() {
         return VERSION;
     }
 
     /**
-     * Returns the byte length that will store the version number
-     
-     */
-    public static int getVersionLength() {
-        return VERSION_LENGTH;
-    }
-
-    /**
-     * Returns the byte length of the magic bytes
-     
-     */
-    public static int getMagicBytesLength() {
-        return MAGIC_LENGTH;
-    }
-
-    /**
-     * Returns the byte length of the initial vector
-     
-     */
-    public static int getIvLength() {
-        return IV_LENGTH;
-    }
-
-    /**
      * Returns the iterations used for deriving the combined key from
      * the text password
-     
      */
     public static int getIterations() {
-        return ITERATIONS;
+        return iterations;
     }
 
-    /**
-     * Returns the byte length of the iterations that will be stored in the config file
-     
-     */
-    public static int getIterationsLength() {
-        return ITERATIONS_LENGTH;
-    }
-
-    /**
-     * Returns the byte length of the HMAC key that will be stored in the file
-     
-     */
-    public static int getHMACKeyLength() {
-        return HMAC_KEY_LENGTH;
-    }
-
-    /**
-     * Returns the byte length of the encryption key
-     
-     */
-    public static int getKeyLength() {
-        return KEY_LENGTH;
+    public static void setIterations(int iterations) {
+        SalmonGenerator.iterations = iterations;
     }
 
     /**
      * Returns a secure random byte array
-     
      */
     public static byte[] getSecureRandomBytes(int size) {
         SecureRandom keyrnd = new SecureRandom();
@@ -172,55 +139,44 @@ public class SalmonGenerator {
 
     /**
      * Generates a secure random combined key (encryption key + HMAC key)
-     
      */
     public static byte[] generateCombinedKey() {
-        return getSecureRandomBytes(getCombinedKeyLength());
+        return getSecureRandomBytes(COMBINED_KEY_LENGTH);
     }
 
     /**
      * Derives the key from a text password
-     * @param pass The text password to be used
-     * @param salt The salt to be used for the key derivation
+     *
+     * @param pass       The text password to be used
+     * @param salt       The salt to be used for the key derivation
      * @param iterations The number of iterations the key derivation alogrithm will use
-     
      */
     public static byte[] getMasterKey(String pass, byte[] salt, int iterations) throws Exception {
-        byte[] masterKey = SalmonGenerator.getKeyFromPassword(pass, salt, iterations, getMasterKeyLength());
+        byte[] masterKey = SalmonGenerator.getKeyFromPassword(pass, salt, iterations, MASTER_KEY_LENGTH);
         return masterKey;
     }
 
     /**
-     * Return the length of the master key in bytes
-     
-     */
-    public static int getMasterKeyLength() {
-        return MASTER_KEY_LENGTH;
-    }
-
-    /**
      * Generates a salt
-     
      */
     public static byte[] generateSalt() {
-        return getSecureRandomBytes(getSaltLength());
+        return getSecureRandomBytes(SALT_LENGTH);
     }
 
     /**
      * Generates the initial vector used that will be used with the master key to encrypt the combined key
-     
      */
     public static byte[] generateMasterKeyIV() {
-        return getSecureRandomBytes(getIvLength());
+        return getSecureRandomBytes(IV_LENGTH);
     }
 
     /**
      * Function will derive a key from a text password using Pbkdf2 with SHA256
-     * @param password The password that will be used to derive the key
-     * @param salt The salt byte array that will be used together with the password
-     * @param iterations The iterations to be used with Pbkdf2
+     *
+     * @param password    The password that will be used to derive the key
+     * @param salt        The salt byte array that will be used together with the password
+     * @param iterations  The iterations to be used with Pbkdf2
      * @param outputBytes The number of bytes for the key
-     
      */
     public static byte[] getKeyFromPassword(String password, byte[] salt, int iterations, int outputBytes) throws Exception {
         //PBKDF2WithHmacSHA256 might not available for some devices
@@ -233,28 +189,41 @@ public class SalmonGenerator {
     }
 
     /**
-     * Returns the HMAC signature length
-     
+     * Increase the sequential NONCE by a value of 1.
+     * This implementation assumes that the NONCE length is 8 bytes or less so it can fit in a long.
+     *
+     * @param vaultNonce
+     * @param maxVaultNonce
+     * @return
+     * @throws Exception
      */
-    public static int getHmacResultLength() {
-        return HMAC_RESULT_LENGTH;
+    public static byte[] increaseNonce(byte[] vaultNonce, byte[] maxVaultNonce) throws Exception {
+        long nonce = BitConverter.toLong(vaultNonce, 0, NONCE_LENGTH);
+        long maxNonce = BitConverter.toLong(maxVaultNonce, 0, NONCE_LENGTH);
+        nonce++;
+        if (nonce <= 0 || nonce > maxNonce)
+            throw new Exception("Cannot import file, vault exceeded maximum nonces");
+        return BitConverter.toBytes(nonce, 8);
     }
+
 
     /**
-     * Returns the HMAC signature length
-     
+     * Returns the middle nonce in the provided range.
+     * Note: This assumes the nonce is 8 bytes, if you need to increase the nonce length
+     * then the long transient variables will not hold and you will need to
+     * override with your own implementation.
+     *
+     * @param startNonce
+     * @param endNonce
+     * @return
      */
-    public static int getBlockSize() {
-        return BLOCK_SIZE;
+    public static byte[] splitNonceRange(byte[] startNonce, byte[] endNonce) throws Exception {
+        long start = BitConverter.toLong(startNonce, 0, SalmonGenerator.NONCE_LENGTH);
+        long end = BitConverter.toLong(endNonce, 0, SalmonGenerator.NONCE_LENGTH);
+        // we reserve some nonces
+        if(end - start < 256)
+            throw new Exception("Not enough nonces left");
+        return BitConverter.toBytes(start + (end - start) / 2, SalmonGenerator.NONCE_LENGTH);
     }
-
-    /**
-     * Returns the Vault Nonce Length
-     
-     */
-    public static int getNonceLength() {
-        return NONCE_LENGTH;
-    }
-
 }
 
