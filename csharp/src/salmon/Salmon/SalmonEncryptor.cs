a1ba5fc294db1f9c93b3e1655591ac1e42289bb7
mku11 <mku11@example.com>
2023-03-11 18:43:01 +0200

    parallel processing

--- csharp/src/salmon/Salmon/SalmonEncryptor.cs
+++ csharp/src/salmon/Salmon/SalmonEncryptor.cs
@@ -24,6 +24,7 @@ SOFTWARE.
 using Salmon.Streams;
 using System;
 using System.IO;
+using System.Threading;
 using static Salmon.Streams.SalmonStream;
 
 namespace Salmon
@@ -33,81 +34,312 @@ namespace Salmon
     /// </summary>
     public class SalmonEncryptor
     {
-        private static readonly int ENC_BUFFER_SIZE = 32768;
+        private static int bufferSize = 32768;
 
         /// <summary>
-        /// Decrypts a text string 
+        /// Decrypts a byte array,
         /// </summary>
-        /// <param name="text">Text to be decrypted</param>
+        /// <param name="data">Data to be decrypted</param>
         /// <param name="key">The encryption key to be used</param>
         /// <param name="nonce">The nonce to be used</param>
+        /// <param name="header">Optional header data to be added to the header (ie store Nonce). Header data are not encrypted!</param>
+        /// <param name="threads">Parallel processing threads default is a single thread.</param>
         /// <returns></returns>
-        // TODO: there is currently no integrity for filenames, is it worthy it?
-        public static byte[] Decrypt(byte[] text, byte[] key, byte[] nonce, bool header)
+        public static byte[] Decrypt(byte[] data, byte[] key, byte[] nonce = null, 
+            bool hasHeaderData = false, int threads = 1,
+            bool integrity = false, byte[] hmacKey = null, int? chunkSize = null)
         {
             if (key == null)
                 throw new Exception("Need to specify a key");
-            if (!header && nonce == null)
+            if (!hasHeaderData && nonce == null)
                 throw new Exception("Need to specify a nonce if the file doesn't have a header");
+            
+            if (integrity)
+                chunkSize = chunkSize == null ? SalmonStream.DEFAULT_CHUNK_SIZE : (int)chunkSize;
 
-            MemoryStream inputStream = new MemoryStream(text);
+            MemoryStream inputStream = new MemoryStream(data);
             byte[] headerData = null;
-            if (header)
+            if (hasHeaderData)
             {
-                byte[] magicBytes = new byte[SalmonGenerator.GetMagicBytesLength()];
+                byte[] magicBytes = new byte[SalmonGenerator.MAGIC_LENGTH];
                 inputStream.Read(magicBytes, 0, magicBytes.Length);
-                byte[] versionBytes = new byte[SalmonGenerator.GetVersionLength()];
+                byte[] versionBytes = new byte[SalmonGenerator.VERSION_LENGTH];
                 inputStream.Read(versionBytes, 0, versionBytes.Length);
-                nonce = new byte[SalmonGenerator.GetNonceLength()];
+                byte[] chunkSizeHeader = new byte[SalmonGenerator.CHUNKSIZE_LENGTH];
+                inputStream.Read(chunkSizeHeader, 0, chunkSizeHeader.Length);
+                chunkSize = (int) BitConverter.ToLong(chunkSizeHeader, 0, SalmonGenerator.CHUNKSIZE_LENGTH);
+                if (chunkSize > 0)
+                    integrity = true;
+                nonce = new byte[SalmonGenerator.NONCE_LENGTH];
                 inputStream.Read(nonce, 0, nonce.Length);
-
                 inputStream.Position = 0;
-                headerData = new byte[magicBytes.Length + versionBytes.Length + nonce.Length];
+                headerData = new byte[magicBytes.Length + versionBytes.Length + chunkSizeHeader.Length + nonce.Length];
                 inputStream.Read(headerData, 0, headerData.Length);
             }
-            SalmonStream stream = new SalmonStream(key, nonce, EncryptionMode.Decrypt, inputStream,
-                headerData: headerData);
-            byte[] buffer = new byte[ENC_BUFFER_SIZE];
-            int bytesRead = stream.Read(buffer, 0, buffer.Length);
-            inputStream.Close();
-            stream.Close();
-            byte[] decBytes = new byte[bytesRead];
-            Array.Copy(buffer,0, decBytes, 0,bytesRead);
-            return decBytes;
 
+            long realSize = GetOutputSize(data, key, nonce, EncryptionMode.Decrypt,
+                headerData: headerData, integrity: integrity, chunkSize: chunkSize, hmacKey: hmacKey);
+            byte[] outData = new byte[realSize];
+
+            if (threads == 1)
+            {
+                DecryptData(inputStream, 0, inputStream.Length, outData,
+                    key, nonce, headerData, integrity, hmacKey, chunkSize);
+                return outData;
+            }
+
+            AutoResetEvent done = new AutoResetEvent(false);
+            int runningThreads = 1;
+            long partSize = data.Length;
+
+            // if we want to check integrity we align to the HMAC Chunk size otherwise to the AES Block
+            long minPartSize = SalmonStream.AES_BLOCK_SIZE;
+            if (integrity && chunkSize != null)
+                minPartSize = (long)chunkSize;
+            else if (integrity)
+                minPartSize = SalmonStream.DEFAULT_CHUNK_SIZE;
+
+            if (partSize > minPartSize)
+            {
+                partSize = (int)Math.Ceiling(partSize / (float)threads);
+                // if we want to check integrity we align to the HMAC Chunk size instead of the AES Block
+                long rem = partSize % minPartSize;
+                if (rem != 0)
+                    partSize += minPartSize - rem;
+
+                runningThreads = (int)(data.Length / partSize);
+                if (partSize % data.Length != 0)
+                    runningThreads++;
+            }
+
+            Exception ex = null;
+            for (int i = 0; i < runningThreads; i++)
+            {
+                int index = i;
+                ThreadPool.QueueUserWorkItem(state =>
+                {
+                    try
+                    {
+                        long start = partSize * index;
+                        long length = Math.Min(partSize, data.Length - start);
+                        MemoryStream ins = new MemoryStream(data);
+                        DecryptData(ins, start, length, outData, key, nonce, headerData, integrity, hmacKey, chunkSize);
+                    }
+                    catch (Exception ex1)
+                    {
+                        ex = ex1;
+                    }
+                    runningThreads--;
+                    if (runningThreads == 0)
+                        done.Set();
+                });
+            }
+            done.WaitOne();
+            inputStream.Close();
+            if (ex != null)
+                throw ex;
+            return outData;
+        }
+
+        public static long GetOutputSize(byte[] data, byte[] key, byte[] nonce, EncryptionMode mode, 
+            byte[] headerData, bool integrity, int? chunkSize, byte[] hmacKey)
+        {
+            MemoryStream inputStream = new MemoryStream(data);
+            SalmonStream s = new SalmonStream(key, nonce, mode, inputStream,
+                headerData: headerData, integrity: integrity, chunkSize: chunkSize, hmacKey: hmacKey);
+            long size = s.ActualLength;
+            s.Close();
+            return size;
+        }
+
+        private static void DecryptData(Stream inputStream, long start, long count, byte[] outData,
+            byte[] key, byte[] nonce,
+            byte[] headerData = null, bool integrity = false, byte[] hmacKey = null, int? chunkSize = null)
+        {
+            SalmonStream stream = null;
+            MemoryStream outputStream = null;
+            try
+            {
+                Console.WriteLine("Decrypting range: " + start + " to: " + count);
+                outputStream = new MemoryStream(outData);
+                outputStream.Position = start;
+                stream = new SalmonStream(key, nonce, EncryptionMode.Decrypt, inputStream,
+                headerData: headerData, integrity: integrity, chunkSize: chunkSize, hmacKey: hmacKey);
+                stream.Position = start;
+                long totalChunkBytesRead = 0;
+                // need to be align to the chunksize if there is one
+                int buffSize = Math.Max(bufferSize, stream.GetChunkSize());
+                byte[] buff = new byte[buffSize];
+                int bytesRead;
+                while ((bytesRead = stream.Read(buff, 0, Math.Min(buff.Length, (int)(count - totalChunkBytesRead)))) > 0
+                    && totalChunkBytesRead < count)
+                {
+                    outputStream.Write(buff, 0, bytesRead);
+                    totalChunkBytesRead += bytesRead;
+                }
+                outputStream.Flush();
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+                throw ex;
+            }
+            finally
+            {
+                if(inputStream!=null)
+                    inputStream.Close();
+                if (stream != null)
+                    stream.Close();
+                if (outputStream != null)
+                    outputStream.Close();
+            }
         }
 
         /// <summary>
         /// Encrypts a byte array
         /// </summary>
-        /// <param name="text">Text to be encrypted</param>
+        /// <param name="data">Text to be encrypted</param>
         /// <param name="key">The encryption key to be used</param>
         /// <param name="nonce">The nonce to be used</param>
         /// <returns></returns>
-        public static byte[] Encrypt(byte[] text, byte[] key, byte[] nonce, bool header)
+        public static byte[] Encrypt(byte[] data, byte[] key, byte[] nonce = null, 
+            bool storeHeaderData = false, int threads = 1,
+            bool integrity = false, byte[] hmacKey = null, int? chunkSize = null)
         {
+            if (integrity)
+                chunkSize = chunkSize == null ? SalmonStream.DEFAULT_CHUNK_SIZE : (int)chunkSize;
+            else
+                chunkSize = 0;
+
             MemoryStream outputStream = new MemoryStream();
             byte[] headerData = null;
-            if (header)
+            if (storeHeaderData)
             {
                 byte[] magicBytes = SalmonGenerator.GetMagicBytes();
                 outputStream.Write(magicBytes, 0, magicBytes.Length);
                 byte version = SalmonGenerator.GetVersion();
                 byte[] versionBytes = new byte[] { version };
                 outputStream.Write(versionBytes, 0, versionBytes.Length);
+                byte[] chunkSizeBytes = BitConverter.ToBytes((int) chunkSize, SalmonGenerator.CHUNKSIZE_LENGTH);
+                outputStream.Write(chunkSizeBytes, 0, chunkSizeBytes.Length);
                 outputStream.Write(nonce, 0, nonce.Length);
                 outputStream.Flush();
                 headerData = outputStream.ToArray();
             }
 
-            SalmonStream stream = new SalmonStream(key, nonce, EncryptionMode.Encrypt, outputStream, headerData: headerData);
-            stream.Write(text, 0, text.Length);
-            stream.Flush();
-            
-            byte [] encBytes = outputStream.ToArray();
+            long realSize = GetOutputSize(data, key, nonce, EncryptionMode.Encrypt,
+                headerData: headerData, integrity: integrity, chunkSize: chunkSize, hmacKey: hmacKey);
+            byte[] outData = new byte[realSize];
+            outputStream.Position = 0;
+            outputStream.Read(outData, 0, (int)outputStream.Length);
             outputStream.Close();
-            stream.Close();
-            return encBytes;
+
+            if (threads == 1)
+            {
+                MemoryStream inputStream = new MemoryStream(data);
+                EncryptData(inputStream, 0, data.Length, outData,
+                    key, nonce, headerData, integrity, hmacKey, chunkSize);
+                return outData;
+            }
+
+            AutoResetEvent done = new AutoResetEvent(false);
+            int runningThreads = 1;
+            long partSize = data.Length;
+
+            // if we want to check integrity we align to the HMAC Chunk size otherwise to the AES Block
+            long minPartSize = SalmonStream.AES_BLOCK_SIZE;
+            if (integrity && chunkSize != null)
+                minPartSize = (long)chunkSize;
+            else if (integrity)
+                minPartSize = SalmonStream.DEFAULT_CHUNK_SIZE;
+
+            if (partSize > minPartSize)
+            {
+                partSize = (int)Math.Ceiling(partSize / (float)threads);
+                // if we want to check integrity we align to the HMAC Chunk size instead of the AES Block
+                long rem = partSize % minPartSize;
+                if (rem != 0)
+                    partSize += minPartSize - rem;
+
+                runningThreads = (int)(data.Length / partSize);
+                if (partSize % data.Length != 0)
+                    runningThreads++;
+            }
+
+            Exception ex = null;
+            for (int i = 0; i < runningThreads; i++)
+            {
+                int index = i;
+                ThreadPool.QueueUserWorkItem(state =>
+                {
+                    try
+                    {
+                        long start = partSize * index;
+                        long length = Math.Min(partSize, data.Length - start);
+                        MemoryStream ins = new MemoryStream(data);
+                        EncryptData(ins, start, length, outData, key, nonce, headerData, integrity, hmacKey, chunkSize);
+                    }
+                    catch (Exception ex1)
+                    {
+                        ex = ex1;
+                    }
+                    runningThreads--;
+                    if (runningThreads == 0)
+                        done.Set();
+                });
+            }
+            done.WaitOne();
+            if (ex != null)
+                throw ex;
+            return outData;
+        }
+
+        private static void EncryptData(MemoryStream inputStream, long start, long count, byte[] outData,
+            byte[] key, byte[] nonce,byte[] headerData = null, 
+            bool integrity = false, byte[] hmacKey = null, int? chunkSize = null)
+        {
+            MemoryStream outputStream = new MemoryStream(outData);
+            SalmonStream stream = null;
+            try
+            {
+                Console.WriteLine("Encrypting range: " + start + " to: " + count);
+                inputStream.Position = start;
+                stream = new SalmonStream(key, nonce, EncryptionMode.Encrypt, outputStream, headerData: headerData, 
+                    integrity: integrity, chunkSize: chunkSize, hmacKey: hmacKey);
+                stream.SetAllowRangeWrite(true);
+                stream.Position = start;
+                long totalChunkBytesRead = 0;
+                // need to be align to the chunksize if there is one
+                int buffSize = Math.Max(bufferSize, stream.GetChunkSize());
+                byte[] buff = new byte[buffSize];
+                int bytesRead;
+                while ((bytesRead = inputStream.Read(buff, 0, Math.Min(buff.Length, (int)(count - totalChunkBytesRead)))) > 0
+                    && totalChunkBytesRead < count)
+                {
+                    stream.Write(buff, 0, bytesRead);
+                    totalChunkBytesRead += bytesRead;
+                }
+                stream.Flush();
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+                throw ex;
+            }
+            finally
+            {
+                if (outputStream != null)
+                    outputStream.Close();
+                if (stream != null)
+                    stream.Close();
+                if (inputStream != null)
+                    inputStream.Close();
+            }
+        }
+
+        public static void SetBufferSize(int bufferSize)
+        {
+            SalmonEncryptor.bufferSize = bufferSize;
         }
     }
 }
