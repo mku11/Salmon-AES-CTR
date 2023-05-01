76d23d2886297df89de19530f072b57092f014c0
mku11 <mku11@example.com>
2023-05-01 09:24:08 +0300

    deploy libs in separate jars

--- javafx/app/scripts/start.bat
+++ javafx/app/scripts/start.bat
@@ -4,4 +4,6 @@ REM If you use AES intrinsics mek sure this file and salmon.dll are in the same 
 :: set the javafx library path
 set JAVA_FX_PATH="D:\tools\javafx-sdk-19\lib"
 
-java --module-path %JAVA_FX_PATH% --add-modules=javafx.controls --add-modules=javafx.swing --add-modules=javafx.fxml --add-modules=javafx.media -jar salmon-java-app.jar
+set JAVA_FX_MODULES=--add-modules=javafx.controls --add-modules=javafx.swing --add-modules=javafx.fxml --add-modules=javafx.media
+
+java --module-path %JAVA_FX_PATH% %JAVA_FX_MODULES% -cp "./*" com.mku11.salmon.vault.main.Main
