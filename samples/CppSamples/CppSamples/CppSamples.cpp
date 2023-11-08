d904d652b27452b795fbc2ec6476a60579dc8285
mku11 <mku11@example.com>
2023-11-08 09:05:16 +0200

    cpp samples

---
+++ samples/CppSamples/CppSamples/CppSamples.cpp
@@ -0,0 +1,74 @@
+#include <windows.h>
+#include "wincrypt.h"
+#include <iostream>
+#include <cstring>
+extern "C" {
+#include "salmon.h"
+using namespace std;
+}
+
+int main()
+{
+
+	HCRYPTPROV   hCryptProv;
+	BYTE         key[32];
+	BYTE         nonce[8];
+	CryptAcquireContextW(&hCryptProv, 0, 0, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT);
+	CryptGenRandom(hCryptProv, 32, key); // get a random key
+	CryptGenRandom(hCryptProv, 8, nonce); // 8 bytes for the random nonce
+
+	// choose the implementation:
+	int implType = AES_IMPL_AES_INTR; // or use AES_IMPL_TINY_AES
+
+	// initialize
+	salmon_init(implType);
+
+	// set up the encryption key
+	uint8_t* encKey = NULL;
+	if (implType == AES_IMPL_TINY_AES)
+	{
+		encKey = key;
+	}
+	else
+	{
+		// or if we use the intrinsics we can expand the key
+		salmon_init(AES_IMPL_AES_INTR);
+		uint8_t expandedKey[240];
+		salmon_expandKey(key, expandedKey);
+		encKey = expandedKey;
+	}
+
+	// The text to encrypt:
+	string text = "This is a plaintext that will be used for testing";
+	char const* bytes = text.c_str();
+	int length = strlen(bytes);
+	cout << bytes << endl;
+	uint8_t* origPlainText = (uint8_t*)bytes;
+
+	// set the counter
+	BYTE	counter[16];
+	memset(counter, 0, 16);
+	memcpy(counter, nonce, 8);
+	
+	// encrypt the byte array
+	uint8_t encText[1024];
+	int bytesEncrypted = salmon_transform(
+		encKey, counter, AES_MODE_ENCRYPTION,
+		origPlainText, 0,
+		encText, 0, length);
+
+	// reset the counter
+	memset(counter, 0, 16);
+	memcpy(counter, nonce, 8);
+	
+	uint8_t plainText[1024];
+	// decrypt the byte array
+	int bytesDecrypted = salmon_transform(
+		encKey, counter, AES_MODE_DECRYPTION,
+		encText, 0,
+		plainText, 0, length);
+
+	// this is the decrypted string
+	string decText = string((char*)plainText, bytesDecrypted);
+	cout << decText.c_str() << endl;
+}
