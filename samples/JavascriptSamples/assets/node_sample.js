df60992db5f37f675dfd9fd580dca23337a9a5b7
mku11 <mku11@example.com>
2024-04-06 17:17:17 +0300

    fixed worker path

--- samples/JavascriptSamples/assets/node_sample.js
+++ samples/JavascriptSamples/assets/node_sample.js
@@ -1,4 +1,8 @@
 import { Sample } from './common.js';
 import { JsNodeFile } from '../lib/salmon-fs/file/js_node_file.js';
+import { SalmonEncryptor } from '../lib/salmon-core/salmon/salmon_encryptor.js';
+import { SalmonDecryptor } from '../lib/salmon-core/salmon/salmon_decryptor.js';
 
+SalmonEncryptor.setWorkerPath('../lib/salmon-core/salmon/salmon_decryptor_worker.js');
+SalmonDecryptor.setWorkerPath('../lib/salmon-core/salmon/salmon_decryptor_worker.js');
 await Sample.streamSamples();
