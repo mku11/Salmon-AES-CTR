e05804c9bda221c5250b245a506501a35580ca44
mku11 <93253421+mku11@users.noreply.github.com>
2024-09-17 15:49:30 +0300

    norm path

--- libs/projects/salmon-libs-gradle/salmon-ws/scripts/start-salmon-ws.bat
+++ libs/projects/salmon-libs-gradle/salmon-ws/scripts/start-salmon-ws.bat
@@ -5,7 +5,7 @@
    java -cp %CURRDIR%salmon-ws-2.1.0.war org.springframework.boot.loader.WarLauncher %1
  ) else (
    echo You need to pass an argument for the location of the drive
-   echo ie: start-salmon-ws.bat "D:\\pathto\\mydrive"
+   echo ie: start-salmon-ws.bat "D:\pathto\mydrive"
    set /p key="Press any key to exit"
  )
 
