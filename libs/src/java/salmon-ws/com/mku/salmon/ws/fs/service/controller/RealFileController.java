c61c2af216081068c7ee51ee3dfc9e381c6b9bed
mku11 <93253421+mku11@users.noreply.github.com>
2024-09-18 16:41:21 +0300

    fixed examples and upload

--- libs/src/java/salmon-ws/com/mku/salmon/ws/fs/service/controller/RealFileController.java
+++ libs/src/java/salmon-ws/com/mku/salmon/ws/fs/service/controller/RealFileController.java
@@ -2,9 +2,16 @@ package com.mku.salmon.ws.fs.service.controller;
 
 import com.mku.file.IRealFile;
 import com.mku.salmon.ws.fs.service.FileSystem;
+import com.mku.streams.InputStreamWrapper;
+import com.mku.streams.RandomAccessStream;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -22,7 +29,7 @@ public class RealFileController {
     /**
      * Get details about a file<br>
      * example:
-     * curl -X GET "http://localhost:8080/api/info?path=/U0xNAgAAAABAAAAAAAADGOKoJtY0"
+     * curl -X GET "http://localhost:8080/api/info?path=/fs/U0xNAgAAAABAAAAAAAADGOKoJtY0"
      * @param path The file path
      * @return
      */
@@ -34,7 +41,7 @@ public class RealFileController {
     /**
      * List files and directories under a directory<br>
      * example:
-     * curl -X GET "http://localhost:8080/api/list?path=/U0xNAgAAAABAAAAAAAADGOKoJtY0"
+     * curl -X GET "http://localhost:8080/api/list?path=/fs/U0xNAgAAAABAAAAAAAADGOKoJtY0"
      * @param path The directory path
      * @return
      */
@@ -55,7 +62,7 @@ public class RealFileController {
     /**
      * Create a directory<br>
      * example:
-     * curl -X POST "http://localhost:8080/api/mkdir?path=/U0xNAgAAAABAAAAAAAADGOKoJtY0"
+     * curl -X POST "http://localhost:8080/api/mkdir?path=/fs/U0xNAgAAAABAAAAAAAADGOKoJtY0"
      * @param path The directory path
      * @return
      */
@@ -67,40 +74,67 @@ public class RealFileController {
         return new RealFileNode(file);
     }
 
+
+    /**
+     * Create a file<br>
+     * example:
+     * curl -X POST "http://localhost:8080/api/mkdir?path=/fs/U0xNAgAAAABAAAAAAAADGOKoJtY0"
+     * @param path The directory path
+     * @return
+     */
+    @PostMapping("/create")
+    public RealFileNode create(String path) throws IOException {
+        IRealFile file = FileSystem.getFile(path);
+        IRealFile parent = file.getParent();
+        file = parent.createFile(file.getBaseName());
+        return new RealFileNode(file);
+    }
+
     /**
      * Upload a file under a directory<br>
      * example:
-     * curl -X POST -F "file=@D:/tmp/testdata/image.jpg" "http://localhost:8080/api/upload?destDir=/U0xNAgAAAABAAAAAAAACB5s0xrH2KAs="
+     * curl -X POST -F "file=@D:/tmp/testdata/data.dat" "http://localhost:8080/api/upload?path=/fs/U0xNAgAAAABAAAAAAAACB5s0xrH2KAs="
      *
-     * @param file The file
-     * @param destDir The destination directory
+     * @param file The file data
+     * @param path The path to the file
      * @return
      * @throws IOException
      */
     @PostMapping("/upload")
-    public RealFileNode upload(@RequestParam("file") MultipartFile file, String destDir) throws IOException {
-        IRealFile rFile = FileSystem.copy(destDir, file);
+    public RealFileNode upload(@RequestParam("file") MultipartFile file, String path) throws IOException {
+        IRealFile rFile = FileSystem.write(path, file);
         return new RealFileNode(rFile);
+    }
+
+    @GetMapping(path = "/get")
+    public ResponseEntity<Resource> get(String path) throws IOException {
+        IRealFile rFile = FileSystem.getFile(path);
+        InputStreamWrapper inputStreamWrapper = new InputStreamWrapper(rFile.getInputStream());
+        InputStreamResource resource = new InputStreamResource(inputStreamWrapper);
+        return ResponseEntity.ok()
+                .contentLength(rFile.length())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 
     /**
      * Copy a file to the destination directory<br>
      * example:
-     *  curl -X PUT "http://localhost:8080/api/copy?sourcePath=/U0xNAgAAAABAAAAAAAAA5hTh7E6oVTYJzH0=&destDir=/U0xNAgAAAABAAAAAAAACB5s0xrH2KAs="
+     *  curl -X PUT "http://localhost:8080/api/copy?sourcePath=/U0xNAgAAAABAAAAAAAAA5hTh7E6oVTYJzH0=&destDir=/fs/U0xNAgAAAABAAAAAAAACB5s0xrH2KAs=&filename=U0xNAgAAAABAAAAAAAACB5s0xrH2KA1"
      * @param sourcePath The file to copy
      * @param destDir The destination directory
      * @return
      * @throws IOException
      */
     @PostMapping("/copy")
-    public RealFileNode copy(String sourcePath, String destDir) throws IOException {
+    public RealFileNode copy(String sourcePath, String destDir, String filename) throws IOException {
         IRealFile source = FileSystem.getFile(sourcePath);
         IRealFile dest = FileSystem.getFile(destDir);
         IRealFile nFile;
         if (source.isDirectory()) {
             throw new IOException("Cannot copy directories, use createDirectory and copy recursively");
         } else
-            nFile = source.copy(dest);
+            nFile = source.copy(dest, filename);
         return new RealFileNode(nFile);
     }
 
@@ -108,28 +142,29 @@ public class RealFileController {
     /**
      * Move a file to the destination directory<br>
      * example:
-     *  curl -X PUT "http://localhost:8080/api/move?sourcePath=/U0xNAgAAAABAAAAAAAAA5hTh7E6oVTYJzH0=&destDir=/U0xNAgAAAABAAAAAAAACB5s0xrH2KAs="
+     *  curl -X PUT "http://localhost:8080/api/move?sourcePath=/fs/U0xNAgAAAABAAAAAAAAA5hTh7E6oVTYJzH0=&destDir=/fs/U0xNAgAAAABAAAAAAAACB5s0xrH2KAs=&filename=U0xNAgAAAABAAAAAAAACB5s0xrH2KA1"
      * @param sourcePath The file to move
      * @param destDir The destination directory
+     * @param filename The new file name (optional)
      * @return
      * @throws IOException
      */
     @PutMapping("/move")
-    public RealFileNode move(String sourcePath, String destDir) throws IOException {
+    public RealFileNode move(String sourcePath, String destDir, String filename) throws IOException {
         IRealFile source = FileSystem.getFile(sourcePath);
         IRealFile dest = FileSystem.getFile(destDir);
         IRealFile nFile;
         if (source.isDirectory()) {
             throw new IOException("Cannot move directories, use createDirectory and move recursively");
         } else
-            nFile = source.move(dest);
+            nFile = source.move(dest, filename);
         return new RealFileNode(nFile);
     }
 
     /**
      * Rename a file or directory<br>
      * example:
-     * curl -X PUT "http://localhost:8080/api/rename?path=/U0xNAgAAAABAAAAAAAADEWyHD6u05Tq-UQ==&filename=U0xNAgAAAABAAAAAAAAA5hTh7E6oVTYJzH0="
+     * curl -X PUT "http://localhost:8080/api/rename?path=/fs/U0xNAgAAAABAAAAAAAADEWyHD6u05Tq-UQ==&filename=U0xNAgAAAABAAAAAAAAA5hTh7E6oVTYJzH0="
      * @param path The file or directory path
      * @param filename The new filename
      * @return
@@ -145,7 +180,7 @@ public class RealFileController {
     /**
      * Delete a file or directory<br>
      * example:
-     * curl -X DELETE "http://localhost:8080/api/delete?path=/U0xNAgAAAABAAAAAAAADGOKoJtY0"
+     * curl -X DELETE "http://localhost:8080/api/delete?path=/fs/U0xNAgAAAABAAAAAAAADGOKoJtY0"
      * @param path The file or directory path
      * @return
      */
@@ -154,5 +189,29 @@ public class RealFileController {
         IRealFile file = FileSystem.getFile(path);
         file.delete();
         return new RealFileNode(file);
+    }
+
+    /**
+     * Set the file length<br>
+     * example:
+     * curl -X PUT "http://localhost:8080/api/rename?path=/fs/U0xNAgAAAABAAAAAAAADEWyHD6u05Tq-UQ==&length=1204"
+     * @param path The file
+     * @param length The new size
+     * @return
+     * @throws FileNotFoundException
+     */
+    @PutMapping("/setLength")
+    public RealFileNode setLength(String path, long length) throws IOException {
+        RandomAccessStream stream = null;
+        try {
+            IRealFile file = FileSystem.getFile(path);
+            stream = file.getOutputStream();
+            stream.setLength(length);
+            return new RealFileNode(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            stream.close();
+        }
     }
 }
