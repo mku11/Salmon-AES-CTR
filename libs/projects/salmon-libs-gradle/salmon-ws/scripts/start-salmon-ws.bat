14f434534ec153f3a7ffe3aa1af24b027d9c6e37
mku11 <93253421+mku11@users.noreply.github.com>
2024-09-24 09:27:35 +0300

    updated version for scripts and deployment

--- libs/projects/salmon-libs-gradle/salmon-ws/scripts/start-salmon-ws.bat
+++ libs/projects/salmon-libs-gradle/salmon-ws/scripts/start-salmon-ws.bat
@@ -2,7 +2,7 @@
  SET CURRDIR=%~dp0
 
  if exist "%1" (
-   java -cp %CURRDIR%salmon-ws-2.1.0.war org.springframework.boot.loader.WarLauncher %1
+   java -cp %CURRDIR%salmon-ws-2.2.0.war org.springframework.boot.loader.WarLauncher %1
  ) else (
    echo You need to pass an argument for the location of the drive
    echo ie: start-salmon-ws.bat "D:\pathto\mydrive"
