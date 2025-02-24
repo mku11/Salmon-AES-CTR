56d0bec7e5477a1283ca32f8a78416a57a93a534
mku11 <93253421+mku11@users.noreply.github.com>
2025-02-24 17:11:11 +0200

    py rename vars

--- libs/src/python/salmon_core/salmon_core/salmon/transform/salmon_native_transformer.py
+++ libs/src/python/salmon_core/salmon_core/salmon/transform/salmon_native_transformer.py
@@ -25,9 +25,11 @@ SOFTWARE.
 from salmon_core.bridge.inative_proxy import INativeProxy
 from salmon_core.salmon.bridge.native_proxy import NativeProxy
 from salmon_core.salmon.transform.salmon_aes256_ctr_transformer import SalmonAES256CTRTransformer
+from salmon_core.salmon.salmon_security_exception import SalmonSecurityException
 
 from typeguard import typechecked
 from threading import RLock
+
 
 @typechecked
 class SalmonNativeTransformer(SalmonAES256CTRTransformer):
@@ -36,30 +38,30 @@ class SalmonNativeTransformer(SalmonAES256CTRTransformer):
     native transformer.
     """
 
-    __nativeProxy: INativeProxy = NativeProxy()
+    __native_proxy: INativeProxy = NativeProxy()
     """
     The native proxy to use for loading libraries for different platforms and operating systems.
     """
 
     __lock_object: RLock = RLock()
-    
-    __implType: int = 1
+
+    __impl_type: int = 1
 
     @staticmethod
     def set_native_proxy(proxy: INativeProxy):
-        SalmonNativeTransformer.__nativeProxy = proxy
+        SalmonNativeTransformer.__native_proxy = proxy
 
     @staticmethod
     def get_native_proxy() -> INativeProxy:
-        return SalmonNativeTransformer.__nativeProxy
+        return SalmonNativeTransformer.__native_proxy
 
-    def __init__(self, implType: int):
+    def __init__(self, impl_type: int):
         """
         Constructs a transformer that will use the native aes implementations
-        :param implType: The AES native implementation see ProviderType enum
+        :param impl_type: The AES native implementation see ProviderType enum
         """
         super().__init__()
-        self.__implType = implType
+        self.__impl_type = impl_type
 
     def init(self, key: bytearray, nonce: bytearray):
         """
@@ -68,7 +70,7 @@ class SalmonNativeTransformer(SalmonAES256CTRTransformer):
         :param nonce: The nonce to use.
         :raises IntegrityException: Thrown when security error
         """
-        self.get_native_proxy().salmon_init(self.__implType)
+        self.get_native_proxy().salmon_init(self.__impl_type)
         expanded_key: bytearray = bytearray(SalmonAES256CTRTransformer.EXPANDED_KEY_SIZE)
         SalmonNativeTransformer.get_native_proxy().salmon_expand_key(key, expanded_key)
         self.set_expanded_key(expanded_key)
@@ -78,44 +80,55 @@ class SalmonNativeTransformer(SalmonAES256CTRTransformer):
                      dest_buffer: bytearray, dest_offset: int, count: int) -> int:
         """
         Encrypt the data.
-        :param srcBuffer: The source byte array.
-        :param srcOffset: The source byte offset.
-        :param destBuffer: The destination byte array.
-        :param destOffset: The destination byte offset.
+        :param src_buffer: The source byte array.
+        :param src_offset: The source byte offset.
+        :param dest_buffer: The destination byte array.
+        :param dest_offset: The destination byte offset.
         :param count: The number of bytes to transform.
         :return: The number of bytes transformed.
         """
 
+        if self.get_key() is None:
+            raise SalmonSecurityException("No key found, run init first")
+        if self.get_counter() is None:
+            raise SalmonSecurityException("No counter found, run init first")
+
         # we block for AES GPU since it's not entirely thread safe
-        if self.__implType == 3:
+        if self.__impl_type == 3:
             with SalmonNativeTransformer.__lock_object:
-                return SalmonNativeTransformer.__nativeProxy.salmon_transform(self.get_expanded_key(), self.get_counter(),
-                                                                      src_buffer, src_offset,
-                                                                      dest_buffer, dest_offset, count)
+                return SalmonNativeTransformer.__native_proxy.salmon_transform(self.get_expanded_key(),
+                                                                               self.get_counter(),
+                                                                               src_buffer, src_offset,
+                                                                               dest_buffer, dest_offset, count)
         else:
-            return SalmonNativeTransformer.__nativeProxy.salmon_transform(self.get_expanded_key(), self.get_counter(),
-                                                                      src_buffer, src_offset,
-                                                                      dest_buffer, dest_offset, count)
+            return SalmonNativeTransformer.__native_proxy.salmon_transform(self.get_expanded_key(), self.get_counter(),
+                                                                           src_buffer, src_offset,
+                                                                           dest_buffer, dest_offset, count)
 
     def decrypt_data(self, src_buffer: bytearray, src_offset: int,
                      dest_buffer: bytearray, dest_offset: int, count: int) -> int:
         """
         Decrypt the data.
-        :param srcBuffer: The source byte array.
-        :param srcOffset: The source byte offset.
-        :param destBuffer: The destination byte array.
-        :param destOffset: The destination byte offset.
+        :param src_buffer: The source byte array.
+        :param src_offset: The source byte offset.
+        :param dest_buffer: The destination byte array.
+        :param dest_offset: The destination byte offset.
         :param count: The number of bytes to transform.
         :return: The number of bytes transformed.
         """
-        
+        if self.get_key() is None:
+            raise SalmonSecurityException("No key found, run init first")
+        if self.get_counter() is None:
+            raise SalmonSecurityException("No counter found, run init first")
+
         # we block for AES GPU since it's not entirely thread safe
-        if self.__implType == 3:
+        if self.__impl_type == 3:
             with SalmonNativeTransformer.__lock_object:
-                return SalmonNativeTransformer.__nativeProxy.salmon_transform(self.get_expanded_key(), self.get_counter(),
-                                                                      src_buffer, src_offset,
-                                                                      dest_buffer, dest_offset, count)
+                return SalmonNativeTransformer.__native_proxy.salmon_transform(self.get_expanded_key(),
+                                                                               self.get_counter(),
+                                                                               src_buffer, src_offset,
+                                                                               dest_buffer, dest_offset, count)
         else:
-            return SalmonNativeTransformer.__nativeProxy.salmon_transform(self.get_expanded_key(), self.get_counter(),
-                                                                      src_buffer, src_offset,
-                                                                      dest_buffer, dest_offset, count)
+            return SalmonNativeTransformer.__native_proxy.salmon_transform(self.get_expanded_key(), self.get_counter(),
+                                                                           src_buffer, src_offset,
+                                                                           dest_buffer, dest_offset, count)
