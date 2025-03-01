3098f72e04c9b31e0945b34ec8837c50af670fc1
mku11 <93253421+mku11@users.noreply.github.com>
2025-03-01 22:21:15 +0200

    fixed java build

--- scripts/deploy/build_java_linux_mac.sh
+++ scripts/deploy/build_java_linux_mac.sh
@@ -2,7 +2,7 @@ CURRDIR=$(pwd)
 
 cd ../../libs/projects/salmon-libs-gradle
 ./gradlew --refresh-dependencies
-./gradlew assembleRelease -x test --rerun-tasks
+./gradlew build -x test --rerun-tasks
 ./gradlew publish --rerun-tasks
 
 cd $CURRDIR
