9d97140efbe50344468263589e88f524a3b5c550
mku11 <93253421+mku11@users.noreply.github.com>
2025-03-03 15:37:15 +0200

    ts pass server url params via jest

--- libs/projects/SalmonLibs.vscode/test/jest.setup.js
+++ libs/projects/SalmonLibs.vscode/test/jest.setup.js
@@ -11,10 +11,29 @@ import { SalmonFSTestHelper, TestMode, TestRunnerMode } from "./salmon-fs/salmon
 // NodeJS: to run in node.js command line (or Visual Code)
 var testMode = TestMode.Node;
 var testRunnerMode = TestRunnerMode.NodeJS;
-
+var testHttpServerURL;
+var testWSServerURL;
 // test dir
 // Make sure the dir root on the Web Service and the HTTP virtual folders
 // point to the correct location
 var testDir = "d:\\tmp\\salmon\\test";
 
+for(let arg of process.argv) {
+    let opt = arg.split("=");
+    if(opt[0] == "TEST_DIR" && opt[1])
+        testDir = opt[1];
+    if(opt[0] == "TEST_MODE" && opt[1])
+        testMode = TestMode[opt[1]];
+    if(opt[0] == "WS_SERVER_URL" && opt[1])
+        SalmonFSTestHelper.WS_SERVER_URL = opt[1];
+	if(opt[0] == "HTTP_SERVER_URL" && opt[1])
+        SalmonFSTestHelper.HTTP_SERVER_URL = opt[1];
+}
+
+
 await SalmonFSTestHelper.setTestParams(testDir, testMode, testRunnerMode);
+console.log("testDir: ", testDir);
+console.log("testMode: ", testMode);
+console.log("http server url: ", SalmonFSTestHelper.HTTP_SERVER_URL);
+console.log("HTTP_VAULT_DIR_URL: ", SalmonFSTestHelper.HTTP_VAULT_DIR_URL);
+console.log("ws server url: ", SalmonFSTestHelper.WS_SERVER_URL);
