2a7fb76656bf9b0e4a8d521a11a3bd55416fcc0b
mku11 <93253421+mku11@users.noreply.github.com>
2025-03-04 15:21:51 +0200

    fixed java ws build scripts

--- libs/projects/salmon-libs-gradle/salmon-ws/package.bat
+++ libs/projects/salmon-libs-gradle/salmon-ws/package.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
-set CURRDIR=%CD%
 set SALMON_VERSION=2.3.0
 
+set CURRDIR=%CD%
 set JAVA_WS=java-ws
 set WS_APP_PROPERTIES=.\config\application.properties
 set WS_SCRIPT_SH=.\scripts\start-salmon-ws.sh
@@ -9,32 +9,23 @@ set WS_SCRIPT_BAT=.\scripts\start-salmon-ws.bat
 set WS_WAR=.\build\libs\salmon-ws-%SALMON_VERSION%.war
 set WS_WAR_NAME=salmon-ws.war
 
-set PACKAGES_DIR=packages
 set CONFIG_DIR=config
 set OUTPUT_ROOT=..\..\..\..\output
-
 set WS_OUTPUT_DIR=%OUTPUT_ROOT%\%JAVA_WS%
 set WS_PACKAGE_NAME=%JAVA_WS%.%SALMON_VERSION%
+set WS_PACKAGE_NAME_ZIP=%JAVA_WS%.%SALMON_VERSION%.zip
 
-
-powershell mkdir -ErrorAction SilentlyContinue %PACKAGES_DIR%
-rd /s /q %PACKAGES_DIR%\%WS_PACKAGE_NAME%
+powershell mkdir -ErrorAction SilentlyContinue %WS_OUTPUT_DIR%
 
 :: Web Service
-
-powershell mkdir -ErrorAction SilentlyContinue %PACKAGES_DIR%\%WS_PACKAGE_NAME%
-
-powershell mkdir -ErrorAction SilentlyContinue %PACKAGES_DIR%\%WS_PACKAGE_NAME%\%CONFIG_DIR%
-copy %WS_SCRIPT_BAT% %PACKAGES_DIR%\%WS_PACKAGE_NAME%
-copy %WS_SCRIPT_SH% %PACKAGES_DIR%\%WS_PACKAGE_NAME%
-copy %WS_WAR% %PACKAGES_DIR%\%WS_PACKAGE_NAME%\%WS_WAR_NAME%
-copy %WS_APP_PROPERTIES% %PACKAGES_DIR%\%WS_PACKAGE_NAME%\%CONFIG_DIR%
-copy README.txt %PACKAGES_DIR%\%WS_PACKAGE_NAME%\README.txt
-cd %PACKAGES_DIR%\%WS_PACKAGE_NAME%
-powershell -command Compress-Archive -Force -DestinationPath ..\%WS_PACKAGE_NAME%.zip -Path *
-cd ..\..\
-powershell mkdir -ErrorAction SilentlyContinue %WS_OUTPUT_DIR%
-copy %PACKAGES_DIR%\%WS_PACKAGE_NAME%.zip %WS_OUTPUT_DIR%
-xcopy /E %PACKAGES_DIR%\%WS_PACKAGE_NAME% %WS_OUTPUT_DIR%
-
+rd /s /q %WS_OUTPUT_DIR%\%WS_PACKAGE_NAME%
+powershell mkdir -ErrorAction SilentlyContinue %WS_OUTPUT_DIR%\%WS_PACKAGE_NAME%
+powershell mkdir -ErrorAction SilentlyContinue %WS_OUTPUT_DIR%\%WS_PACKAGE_NAME%\%CONFIG_DIR%
+copy %WS_SCRIPT_BAT% %WS_OUTPUT_DIR%\%WS_PACKAGE_NAME%
+copy %WS_SCRIPT_SH% %WS_OUTPUT_DIR%\%WS_PACKAGE_NAME%
+copy %WS_WAR% %WS_OUTPUT_DIR%\%WS_PACKAGE_NAME%\%WS_WAR_NAME%
+copy %WS_APP_PROPERTIES% %WS_OUTPUT_DIR%\%WS_PACKAGE_NAME%\%CONFIG_DIR%
+copy README.txt %WS_OUTPUT_DIR%\%WS_PACKAGE_NAME%\README.txt
+cd %WS_OUTPUT_DIR%\%WS_PACKAGE_NAME%
+powershell -command Compress-Archive -Force -DestinationPath ..\%WS_PACKAGE_NAME_ZIP% -Path *
 cd %CURRDIR%
