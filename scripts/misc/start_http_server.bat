c2d1b0626f36ea1d6582ec35db3794a145349c70
mku11 <93253421+mku11@users.noreply.github.com>
2025-03-05 16:02:24 +0200

    chain python http server

--- scripts/misc/start_http_server.bat
+++ scripts/misc/start_http_server.bat
@@ -2,6 +2,6 @@ set CURRDIR=%CD%
 
 powershell mkdir -ErrorAction SilentlyContinue d:\tmp\salmon
 cd d:\tmp\salmon
-pip install rangehttpserver
-python -m RangeHTTPServer
+pip install rangehttpserver & ^
+python -m RangeHTTPServer & ^
 cd %CURRDIR%
