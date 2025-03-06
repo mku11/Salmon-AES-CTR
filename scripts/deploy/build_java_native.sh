0338a03c2e951bb16be6632ed19a9ed7bd3418d7
mku11 <93253421+mku11@users.noreply.github.com>
2025-03-06 22:10:23 +0200

    gradle clean before build

--- scripts/deploy/build_java_native.sh
+++ scripts/deploy/build_java_native.sh
@@ -1,6 +1,7 @@
 CURRDIR=$(pwd)
 
 cd ../../libs/projects/salmon-libs-gradle
+./gradlew clean
 ./gradlew --refresh-dependencies
 ./gradlew build -x test --rerun-tasks
 ./gradlew publish --rerun-tasks
