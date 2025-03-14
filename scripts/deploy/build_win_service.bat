91b14e74bea3d2cfa6a53cb5ff798467a816e1ff
mku11 <93253421+mku11@users.noreply.github.com>
2025-03-14 14:25:21 +0200

    added restore to winservice build script

--- scripts/deploy/build_win_service.bat
+++ scripts/deploy/build_win_service.bat
@@ -3,5 +3,8 @@ set CURRDIR=%CD%
 :: to run type VsDevCmd.bat in the windows console before running this script
 
 cd ..\..\services\winservice\project
+msbuild -t:clean & ^
+msbuild -t:restore & ^
 msbuild /property:Configuration=Release & ^
+msbuild /t:publish /p:PublishProfile=Properties\PublishProfiles\FolderProfile.pubxml /p:Configuration=Release & ^
 cd %CURRDIR%
