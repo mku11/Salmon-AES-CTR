08e75fdefd746175230f587ebdff50e17ff423d5
mku11 <93253421+mku11@users.noreply.github.com>
2025-03-16 18:43:26 +0200

    bundle android with respective language

--- scripts/docs/gendocs.sh
+++ scripts/docs/gendocs.sh
@@ -1,24 +1,19 @@
 # use Linux or WSL or cygwin to run
 
-# java
-find ../../libs/src/java -type f -name "*.java" | xargs javadoc --no-module-directories -d ../../output/docs/java/html --ignore-source-errors
+# Java and Android
+find ../../libs/src -type f -name "*.java" | xargs javadoc --no-module-directories -d ../../output/docs/java/html --ignore-source-errors
 
-find ../../libs/src/android -type f -name "*.java" | xargs javadoc --no-module-directories -d ../../output/docs/android/html --ignore-source-errors
-
-# javascript
+# JavaScript
 npx jsdoc ../../libs/projects/SalmonLibs.vscode/lib -r -d ../../output/docs/javascript/html
 
-# typescript
+# TypeScript
 CURR_DIR=$(pwd)
 cd ../../libs/projects/SalmonLibs.vscode
 npm run tsdocs
 cd $CURR_DIR
 
-# .NET
+# .NET and Android .NET
 doxygen Doxyfile.Net
-
-# .NET android
-doxygen DoxyfileAndroid.Net
 
 # Python
 doxygen DoxyfilePython
