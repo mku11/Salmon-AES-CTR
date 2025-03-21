c78d010322a60dd57ae4f2ef1e341304567009ce
mku11 <93253421+mku11@users.noreply.github.com>
2025-03-21 14:06:24 +0200

    java test fixed unix scripts

--- scripts/tests/java/linux_mac/test_java_fs.sh
+++ scripts/tests/java/linux_mac/test_java_fs.sh
@@ -1,9 +1,10 @@
 CURRDIR=$(pwd)
 
 export TEST_DIR="/tmp/salmon/test"
-# export ENABLE_GPU=false
 export TEST_MODE=Local
 
-cd ../../libs/projects/salmon-libs-gradle
+cd ../../../../libs/projects/salmon-libs-gradle
+
 ./gradlew :salmon-fs:test --tests "com.mku.salmon.test.SalmonFSTests" -DTEST_DIR=$TEST_DIR -DTEST_MODE=$TEST_MODE -DENABLE_GPU=$ENABLE_GPU -i --rerun-tasks
+
 cd $CURRDIR
--- scripts/tests/java/linux_mac/test_java_httpfs.sh
+++ scripts/tests/java/linux_mac/test_java_httpfs.sh
@@ -2,9 +2,10 @@ CURRDIR=$(pwd)
 
 export HTTP_SERVER_URL=http://localhost:8000
 export TEST_DIR="/tmp/salmon/test"
-# export ENABLE_GPU=false
 export TEST_MODE=Http
 
-cd ../../libs/projects/salmon-libs-gradle
+cd ../../../../libs/projects/salmon-libs-gradle
+
 ./gradlew :salmon-fs:test --tests "com.mku.salmon.test.SalmonFSHttpTests" -DTEST_DIR=$TEST_DIR -DTEST_MODE=$TEST_MODE -DHTTP_SERVER_URL=$HTTP_SERVER_URL -DENABLE_GPU=$ENABLE_GPU -i --rerun-tasks
+
 cd $CURRDIR
