578afdb3c3354b644d1353e719498a10a80523c7
mku11 <93253421+mku11@users.noreply.github.com>
2025-03-21 17:55:17 +0200

    py fixed test paths

--- scripts/tests/python/linux_mac/test_python_fs.sh
+++ scripts/tests/python/linux_mac/test_python_fs.sh
@@ -6,7 +6,7 @@ export TEST_MODE=Local
 export ENABLE_GPU=false
 export ENC_THREADS=1
 
-cd ../../../libs/test/salmon_fs_test_python
+cd ../../../../libs/test/salmon_fs_test_python
 
 python3 -m unittest -v salmon_fs_tests.SalmonFSTests
 
--- scripts/tests/python/linux_mac/test_python_fs_multi.sh
+++ scripts/tests/python/linux_mac/test_python_fs_multi.sh
@@ -6,7 +6,7 @@ export TEST_MODE=Local
 export ENABLE_GPU=false
 export ENC_THREADS=2
 
-cd ../../../libs/test/salmon_fs_test_python
+cd ../../../../libs/test/salmon_fs_test_python
 
 python3 -m unittest -v salmon_fs_tests.SalmonFSTests
 
