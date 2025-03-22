890112f44c27c288d382c005d71c5ff89c7b6651
mku11 <93253421+mku11@users.noreply.github.com>
2025-03-22 16:01:02 +0200

    fixed gcc for macos

--- scripts/deploy/build_gcc_mac_x86_64.sh
+++ scripts/deploy/build_gcc_mac_x86_64.sh
@@ -2,6 +2,6 @@ CURRDIR=$(pwd)
 
 cd ../../libs/projects/salmon-libs-gcc
 make PLATFORM=mac ARCH=x86_64 USE_OPENCL=1 ENABLE_JNI=1
-make package PLATFORM=mac ARCH=x86_64 USE_OPENCL=1 ENABLE_JNI=1
+make package PLATFORM=macos ARCH=x86_64 USE_OPENCL=1 ENABLE_JNI=1
 
 cd $CURRDIR
