5000f5c5a8506598d9d2106da2b87d1d24133b91
mku11 <93253421+mku11@users.noreply.github.com>
2025-03-22 16:49:38 +0200

    fixed build gcc

--- scripts/deploy/build_gcc_linux_aarch64.sh
+++ scripts/deploy/build_gcc_linux_aarch64.sh
@@ -1,7 +1,6 @@
 CURRDIR=$(pwd)
 
 cd ../../libs/projects/salmon-libs-gcc
-make PLATFORM=linux ARCH=aarch64 USE_OPENCL=1 ENABLE_JNI=1
-make package PLATFORM=linux ARCH=aarch64 USE_OPENCL=1 ENABLE_JNI=1
+make package PLATFORM=linux ARCH=aarch64 USE_OPENCL=1 ENABLE_JNI=1 OPENCL_INCLUDE=/usr/include OPENCL_LIB=/usr/lib/aarch64-linux-gnu
 
 cd $CURRDIR
