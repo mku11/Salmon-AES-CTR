38422c6338ed8680d61a31b71f9f4c2b4a98a1f2
mku11 <93253421+mku11@users.noreply.github.com>
2025-03-23 17:50:33 +0200

    fixed java deploy script

--- scripts/deploy/build_java_nativegpu.sh
+++ scripts/deploy/build_java_nativegpu.sh
@@ -7,8 +7,8 @@ export OPENCL_LIB=/usr/lib/x86_64-linux-gnu/
 cd ../../libs/projects/salmon-libs-gradle
 
 ./gradlew --refresh-dependencies
-./gradlew build -x test --rerun-tasks -DENABLE_GPU=%ENABLE_GPU% -DOPENCL_INCLUDE=%OPENCL_INCLUDE% -DOPENCL_LIB=%OPENCL_LIB% 
-./gradlew publish --rerun-tasks -DENABLE_GPU=%ENABLE_GPU% -DOPENCL_INCLUDE=%OPENCL_INCLUDE% -DOPENCL_LIB=%OPENCL_LIB% 
+./gradlew build -x test -i --rerun-tasks -DENABLE_GPU=$ENABLE_GPU -DOPENCL_INCLUDE=$OPENCL_INCLUDE -DOPENCL_LIB=$OPENCL_LIB 
+./gradlew publish --rerun-tasks -DENABLE_GPU=$ENABLE_GPU -DOPENCL_INCLUDE=$OPENCL_INCLUDE -DOPENCL_LIB=$OPENCL_LIB
 ./package.sh
 
 cd $CURRDIR
