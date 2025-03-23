1e7c2f51b50a396b6bdbfcadeeea7123663f961e
mku11 <93253421+mku11@users.noreply.github.com>
2025-03-23 20:33:43 +0200

    py test added native impls

--- scripts/tests/python/linux_mac/test_python_core_multi.sh
+++ scripts/tests/python/linux_mac/test_python_core_multi.sh
@@ -5,6 +5,13 @@ export ENC_THREADS=2
 
 cd ../../../../libs/test/salmon_core_test_python
 
+export AES_PROVIDER_TYPE=Default
+python3 -m unittest -v salmon_core_tests.SalmonCoreTests
+
+export AES_PROVIDER_TYPE=Aes
+python3 -m unittest -v salmon_core_tests.SalmonCoreTests
+
+export AES_PROVIDER_TYPE=AesIntrinsics
 python3 -m unittest -v salmon_core_tests.SalmonCoreTests
 
 cd $CURRDIR
