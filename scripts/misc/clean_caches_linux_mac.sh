7ca6ff838ed53ec7a530bac94858f5b400a8db2c
mku11 <93253421+mku11@users.noreply.github.com>
2025-03-25 10:38:40 +0200

    misc fixed clearing cache packages

--- scripts/misc/clean_caches_linux_mac.sh
+++ scripts/misc/clean_caches_linux_mac.sh
@@ -7,4 +7,7 @@ rm -rf $HOME/.gradle/caches/modules-2/files-2.1/com.mku.salmon
 # delete nuget caches
 rm -rf $HOME/.nuget/packages/salmon.core
 rm -rf $HOME/.nuget/packages/salmon.fs
+rm -rf $HOME/.nuget/packages/salmon.fs.android
 rm -rf $HOME/.nuget/packages/salmon.native
+rm -rf $HOME/.nuget/packages/salmon.native.android
+rm -rf $HOME/.nuget/packages/salmon.win
--- scripts/misc/clean_caches_win.bat
+++ scripts/misc/clean_caches_win.bat
@@ -7,4 +7,7 @@ rd /s /q %USERPROFILE%\.gradle\caches\modules-2\files-2.1\com.mku.salmon
 :: delete nuget caches
 rd /s /q %USERPROFILE%\.nuget\packages\salmon.core
 rd /s /q %USERPROFILE%\.nuget\packages\salmon.fs
+rd /s /q %USERPROFILE%\.nuget\packages\salmon.fs.android
 rd /s /q %USERPROFILE%\.nuget\packages\salmon.native
+rd /s /q %USERPROFILE%\.nuget\packages\salmon.native.android
+rd /s /q %USERPROFILE%\.nuget\packages\salmon.win
