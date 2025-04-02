3aeb1029821feadab980b027aeb8bb64cdc3888c
mku11 <93253421+mku11@users.noreply.github.com>
2025-04-02 22:07:03 +0300

    force ipv4 for testing

--- scripts/misc/start_http_server.bat
+++ scripts/misc/start_http_server.bat
@@ -3,5 +3,5 @@ set CURRDIR=%CD%
 powershell mkdir -ErrorAction SilentlyContinue d:\tmp\salmon
 cd d:\tmp\salmon
 pip install rangehttpserver & ^
-python -m RangeHTTPServer & ^
+python -m RangeHTTPServer --bind 127.0.0.1 & ^
 cd %CURRDIR%
--- scripts/misc/start_http_server.sh
+++ scripts/misc/start_http_server.sh
@@ -3,5 +3,5 @@ CURRDIR=$(pwd)
 mkdir -p /tmp/salmon
 cd /tmp/salmon
 pip install rangehttpserver
-python3 -m RangeHTTPServer
+python3 -m RangeHTTPServer --bind 127.0.0.1
 cd $CURRDIR
