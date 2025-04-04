77177a2f343e6eb4047dae9b7324606f3076f206
mku11 <93253421+mku11@users.noreply.github.com>
2025-04-04 17:35:30 +0300

    android fixed http fs test cases

--- scripts/tests/android/win/test_android_httpfs.bat
+++ scripts/tests/android/win/test_android_httpfs.bat
@@ -5,7 +5,7 @@ set TEST_MODE=Http
 
 cd ..\..\..\..\libs\projects\salmon-libs-gradle-android
 
-call gradlew.bat :salmon-fs-android-test:connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.class='com.mku.salmon.test.SalmonFSAndroidTests' -DTEST_MODE=%TEST_MODE% -DHTTP_SERVER_URL=%HTTP_SERVER_URL% --rerun-tasks -i
+call gradlew.bat :salmon-fs-android-test:connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.class='com.mku.salmon.test.SalmonFSHttpAndroidTests' -DTEST_MODE=%TEST_MODE% -DHTTP_SERVER_URL=%HTTP_SERVER_URL% --rerun-tasks -i
 if %ERRORLEVEL% GEQ 1 cd %CURRDIR% && EXIT /B 1
 
 cd %CURRDIR%
