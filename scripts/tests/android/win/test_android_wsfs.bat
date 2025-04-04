e64be3fc71f6d8337f98c321130ed994441c8a8e
mku11 <93253421+mku11@users.noreply.github.com>
2025-04-04 17:34:47 +0300

    android fixed test scripts

--- scripts/tests/android/win/test_android_wsfs.bat
+++ scripts/tests/android/win/test_android_wsfs.bat
@@ -5,7 +5,7 @@ set TEST_MODE=WebService
 
 cd ..\..\..\..\libs\projects\salmon-libs-gradle
 
-call gradlew.bat :salmon-fs-android-test:test --tests "com.mku.salmon.test.SalmonFSAndroidTests" -DTEST_MODE=%TEST_MODE% -DWS_SERVER_URL=%WS_SERVER_URL% --rerun-tasks -i
+call gradlew.bat :salmon-fs-android-test:connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.class='com.mku.salmon.test.SalmonFSAndroidTests' -DTEST_MODE=%TEST_MODE% -DWS_SERVER_URL=%WS_SERVER_URL% --rerun-tasks -i
 if %ERRORLEVEL% GEQ 1 cd %CURRDIR% && EXIT /B 1
 
 cd %CURRDIR%
