4ad2591d5914d70a3bc70b6dba83ebf061bb0469
mku11 <93253421+mku11@users.noreply.github.com>
2025-04-08 13:04:58 +0300

    remove http server it's slow

--- scripts/misc/start_http_server.bat
+++
@@ -1,7 +0,0 @@
-set CURRDIR=%CD%
-
-powershell mkdir -ErrorAction SilentlyContinue d:\tmp\salmon
-cd d:\tmp\salmon
-pip install rangehttpserver & ^
-python -m RangeHTTPServer --bind 127.0.0.1 & ^
-cd %CURRDIR%
--- scripts/misc/start_http_server.sh
+++
@@ -1,7 +0,0 @@
-CURRDIR=$(pwd)
-
-mkdir -p /tmp/salmon
-cd /tmp/salmon
-pip install rangehttpserver
-python3 -m RangeHTTPServer --bind 127.0.0.1
-cd $CURRDIR
