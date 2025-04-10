89e5ccac48da9bb7611e52f5a60143978217fdbc
mku11 <93253421+mku11@users.noreply.github.com>
2025-04-10 14:34:10 +0300

    deploy debug

--- scripts/deploy/build_dotnet_native_android.bat
+++ scripts/deploy/build_dotnet_native_android.bat
@@ -3,5 +3,6 @@ set CURRDIR=%CD%
 :: to run type VsDevCmd.bat in the windows console before running this script
 
 cd ..\..\libs\projects\SalmonLibs.VS2022
+:: msbuild /property:Configuration=Debug & ^
 msbuild /property:Configuration=Release & ^
 cd %CURRDIR%
