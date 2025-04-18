f58bbb36179796f328d3fe1b0028306e2b807149
mku11 <93253421+mku11@users.noreply.github.com>
2025-04-18 19:28:44 +0300

    verbose build

--- scripts/deploy/build_android.bat
+++ scripts/deploy/build_android.bat
@@ -2,6 +2,6 @@ set CURRDIR=%CD%
 
 cd ..\..\libs\projects\salmon-libs-gradle-android
 gradlew.bat --refresh-dependencies & ^
-gradlew.bat assembleRelease -x test --rerun-tasks & ^
+gradlew.bat assembleRelease -x test --rerun-tasks -i & ^
 gradlew.bat publish --rerun-tasks & ^
 cd %CURRDIR%
