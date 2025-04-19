d04e7b5f5b8763e8541979ac7003986f4af8f890
mku11 <93253421+mku11@users.noreply.github.com>
2025-04-19 08:31:37 +0300

    java tests fixed provider type

--- scripts/tests/java/win/test_java_native.bat
+++ scripts/tests/java/win/test_java_native.bat
@@ -2,10 +2,10 @@ set CURRDIR=%CD%
 
 cd ..\..\..\..\libs\projects\salmon-libs-gradle
 
-call gradlew.bat :salmon-core:test --tests "com.mku.salmon.test.SalmonNativeTests" -DAES_PROVDER_TYPE=Aes -i --rerun-tasks -i
+call gradlew.bat :salmon-core:test --tests "com.mku.salmon.test.SalmonNativeTests" -DAES_PROVIDER_TYPE=Aes -i --rerun-tasks -i
 if %ERRORLEVEL% GEQ 1 cd %CURRDIR% && EXIT /B 1
 
-call gradlew.bat :salmon-core:test --tests "com.mku.salmon.test.SalmonNativeTests" -DAES_PROVDER_TYPE=AesIntrinsics --rerun-tasks -i
+call gradlew.bat :salmon-core:test --tests "com.mku.salmon.test.SalmonNativeTests" -DAES_PROVIDER_TYPE=AesIntrinsics --rerun-tasks -i
 if %ERRORLEVEL% GEQ 1 cd %CURRDIR% && EXIT /B 1
 
 cd %CURRDIR%
