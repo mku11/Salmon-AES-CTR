f2f6be45bde95a652a5e58afea4c766c0fed6ef5
mku11 <93253421+mku11@users.noreply.github.com>
2025-04-19 10:51:05 +0300

    android sample run correct task

--- scripts/samples/sample_android.bat
+++ scripts/samples/sample_android.bat
@@ -2,7 +2,7 @@ set CURRDIR=%CD%
 
 cd ..\..\samples\AndroidSamples
 
-call gradlew.bat :salmon-android-samples:run --rerun-tasks -i
+call gradlew.bat :salmon-android-samples:installDebug --rerun-tasks -i
 if %ERRORLEVEL% GEQ 1 cd %CURRDIR% && EXIT /B 1
 
 cd %CURRDIR%
