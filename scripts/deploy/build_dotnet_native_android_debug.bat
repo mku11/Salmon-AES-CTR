eb164d2faf3aad3c339dea6a06fd4771b081f812
mku11 <93253421+mku11@users.noreply.github.com>
2025-04-20 16:48:58 +0300

    android debug deploy

---
+++ scripts/deploy/build_dotnet_native_android_debug.bat
@@ -0,0 +1,8 @@
+set CURRDIR=%CD%
+
+:: to run type VsDevCmd.bat in the windows console before running this script
+
+cd ..\..\libs\projects\SalmonLibs.VS2022
+msbuild -t:restore & ^
+msbuild /property:Configuration=Debug & ^
+cd %CURRDIR%
---
+++ scripts/deploy/build_dotnet_nativegpu_android_debug.bat
@@ -0,0 +1,9 @@
+:: Build for x86_64 with GPU support (OpenCL)
+set CURRDIR=%CD%
+
+:: to run type VsDevCmd.bat in the windows console before running this script
+
+cd ..\..\libs\projects\SalmonLibs.VS2022
+msbuild -t:restore & ^
+msbuild /property:Configuration=DebugGPU & ^
+cd %CURRDIR%
