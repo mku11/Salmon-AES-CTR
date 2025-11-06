829fa2a41c5b53e42c15a70e141879e35ef27434
mku11 <93253421+mku11@users.noreply.github.com>
2025-11-06 16:29:47 +0200

    js test scripts: added native providers

--- scripts/tests/javascript/linux_mac/test_javascript_core_multi.sh
+++ scripts/tests/javascript/linux_mac/test_javascript_core_multi.sh
@@ -5,6 +5,10 @@ export ENC_THREADS=2
 
 cd ../../../../libs/projects/SalmonLibs.vscode
 
-npm run test -- salmon-core -t="salmon-core" ENC_THREADS=$ENC_THREADS
+npm run test -- salmon-core -t="salmon-core" AES_PROVIDER_TYPE=Default ENC_THREADS=$ENC_THREADS
+
+npm run test -- salmon-core -t="salmon-core" AES_PROVIDER_TYPE=Aes ENC_THREADS=$ENC_THREADS
+
+npm run test -- salmon-core -t="salmon-core" AES_PROVIDER_TYPE=AesIntrinsics ENC_THREADS=$ENC_THREADS
 
 cd $CURRDIR
