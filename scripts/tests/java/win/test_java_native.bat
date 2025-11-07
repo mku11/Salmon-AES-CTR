a53bb16f58db39ffc47b64b495a44115754690ff
mku11 <93253421+mku11@users.noreply.github.com>
2025-11-07 14:09:20 +0200

    workflow: fixed java native tests

--- scripts/tests/java/win/test_java_native.bat
+++ scripts/tests/java/win/test_java_native.bat
@@ -2,7 +2,7 @@ set CURRDIR=%CD%
 
 cd ..\..\..\..\libs\projects\salmon-libs-gradle
 
-call gradlew.bat :salmon-core:test --tests "com.mku.salmon.test.SalmonNativeTests" -DAES_PROVIDER_TYPE=Aes -i --rerun-tasks -i
+call gradlew.bat :salmon-core:test --tests "com.mku.salmon.test.SalmonNativeTests" -DAES_PROVIDER_TYPE=Aes --rerun-tasks -i
 if %ERRORLEVEL% GEQ 1 cd %CURRDIR% && EXIT /B 1
 
 call gradlew.bat :salmon-core:test --tests "com.mku.salmon.test.SalmonNativeTests" -DAES_PROVIDER_TYPE=AesIntrinsics --rerun-tasks -i
