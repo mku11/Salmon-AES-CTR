41e04e162f1415d90572814d26bdf811246483d3
mku11 <93253421+mku11@users.noreply.github.com>
2025-11-12 15:18:37 +0200

    fixed encode paths

--- scripts/misc/init_gitmodules.bat
+++ scripts/misc/init_gitmodules.bat
@@ -1,21 +1,11 @@
 @ECHO ON
 set CURRDIR=%CD%
 
-:: dependencies
-set SIMPLEIO_VERSION=1.0.1
-set SIMPLEFS_VERSION=1.0.1
-
-:: source settings
-source %CURRDIR%\settings.cfg
-
 :: get submodules
 git submodule update --recursive --init
 
 :: get specific versions
 cd %CURRDIR%\..\..\libs\deps\SimpleIO
-git fetch --tags
-git pull origin main
-git checkout v%SIMPLEIO_VERSION%
 xcopy /E /Y /I src\csharp\SimpleIO ..\..\src\csharp\SimpleIO
 xcopy /E /Y /I src\java\simple-io ..\..\src\java\simple-io
 xcopy /E /Y /I src\python\simple_io ..\..\src\python\simple_io
@@ -23,13 +13,11 @@ xcopy /E /Y /I src\typescript\simple-io ..\..\src\typescript\simple-io
 xcopy /E /Y /I src\android\simple-io ..\..\src\android\simple-io
 
 cd %CURRDIR%\..\..\libs\deps\SimpleFS
-git fetch --tags
-git pull origin main
-git checkout v%SIMPLEFS_VERSION%
 xcopy /E /Y /I src\csharp\SimpleFS ..\..\src\csharp\SimpleFS
 xcopy /E /Y /I src\java\simple-fs ..\..\src\java\simple-fs
 xcopy /E /Y /I src\python\simple_fs ..\..\src\python\simple_fs
 xcopy /E /Y /I src\typescript\simple-fs ..\..\src\typescript\simple-fs
 xcopy /E /Y /I src\android\simple-fs ..\..\src\android\simple-fs
 xcopy /E /Y /I src\dotnetandroid\simple-fs ..\..\src\dotnetandroid\simple-fs
+
 cd %CURRDIR%
--- scripts/misc/init_gitmodules.sh
+++ scripts/misc/init_gitmodules.sh
@@ -1,22 +1,11 @@
 #!/bin/bash -x
 CURRDIR=$(pwd)
 
-# dependencies
-SIMPLEIO_VERSION=1.0.1
-SIMPLEFS_VERSION=1.0.1
-
-# source settings
-dos2unix $CURRDIR/settings.cfg
-source $CURRDIR/settings.cfg
-
 # get submodules
 git submodule update --recursive --init
 
 # get specific versions
 cd $CURRDIR/../../libs/deps/SimpleIO
-git fetch --tags
-git pull origin main
-git checkout v$SIMPLEIO_VERSION
 cp -rf src/csharp/SimpleIO ../../src/csharp/
 cp -rf src/java/simple-io ../../src/java/
 cp -rf src/python/simple_io ../../src/python/
@@ -24,9 +13,6 @@ cp -rf src/typescript/simple-io ../../src/typescript/
 cp -rf src/android/simple-io ../../src/android/
 
 cd $CURRDIR/../../libs/deps/SimpleFS
-git fetch --tags
-git pull origin main
-git checkout v$SIMPLEFS_VERSION
 cp -rf src/csharp/SimpleFS ../../src/csharp/
 cp -rf src/java/simple-fs ../../src/java/
 cp -rf src/python/simple_fs ../../src/python/
