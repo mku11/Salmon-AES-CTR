0d1d007e2cad73256780018df25f5185f8101682
mku11 <93253421+mku11@users.noreply.github.com>
2025-11-28 14:52:15 +0200

    java test: run all non-gpu and win tests

--- scripts/tests/java/win/test_java_ci.bat
+++ scripts/tests/java/win/test_java_ci.bat
@@ -5,6 +5,5 @@ cd %CURRDIR%\..\..\..\misc &^
 START start_http_server.bat &^
 START start_ws_server.bat &^
 TIMEOUT 10 &^
-cd %CURRDIR%\..\..\..\..\libs\projects\salmon-libs-gradle &^
-gradlew.bat test -x :salmon-win:test -DENC_THREADS=1 -DAES_PROVIDER_TYPE=Default -i -DTEST_MODE=WebService &^
+test_java_all.bat &^
 cd %CURRDIR%
