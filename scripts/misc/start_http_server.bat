d7151af4c251cda37f08de20759e1595c904b477
mku11 <93253421+mku11@users.noreply.github.com>
2025-11-28 16:04:06 +0200

    ci

--- scripts/misc/start_http_server.bat
+++ scripts/misc/start_http_server.bat
@@ -3,5 +3,5 @@ set CURRDIR=%CD%
 set TEST_DIR=c:\tmp\salmon
 
 powershell mkdir -ErrorAction SilentlyContinue %TEST_DIR%\test &^
-npx http-server %TEST_DIR% -p 8880 &^
+npx http-server C:\tmp\salmon -p 8880 &^
 cd %CURRDIR%
