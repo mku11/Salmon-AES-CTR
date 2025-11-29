5b4670163548bba93059634822821e69ee372733
mku11 <93253421+mku11@users.noreply.github.com>
2025-11-29 10:29:18 +0200

    ci: run in container

---
+++ scripts/misc/setup_webfs.sh
@@ -0,0 +1,10 @@
+CURRDIR=$(pwd)
+
+cd ../../libs/deps/WebFS/project
+./gradlew bootWar
+cd webfs-service
+./package.sh
+cd ../../output/webfs-service/webfs-service-1.0.0/config
+cp -f ../../../../../../test/config/application.properties.linux_mac.test application.properties
+
+cd $CURRDIR
