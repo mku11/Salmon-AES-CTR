ec3f6a5b609101820628ca0c103ea53d0697c3db
mku11 <93253421+mku11@users.noreply.github.com>
2025-11-30 15:03:17 +0200

    ci: fixed typo

--- scripts/tests/csharp/linux_mac/test_csharp_ci.sh
+++ scripts/tests/csharp/linux_mac/test_csharp_ci.sh
@@ -2,16 +2,16 @@
 CURRDIR=$(pwd)
 
 # ALL
-./test_csharp_core.bat
+./test_csharp_core.sh
 
-./test_csharp_core_multi.bat
+./test_csharp_core_multi.sh
 
-./test_csharp_fs.bat
+./test_csharp_fs.sh
 
-./test_csharp_fs_multi.bat
+./test_csharp_fs_multi.sh
 
-./test_csharp_native.bat
+./test_csharp_native.sh
 
-./test_csharp_native_multi.bat
+./test_csharp_native_multi.sh
 
 cd $CURRDIR
