5977d1b972961d62dc311c2faa7a5528289d872d
mku11 <93253421+mku11@users.noreply.github.com>
2025-11-30 16:13:06 +0200

    ci: fixed bat scripts

--- scripts/tests/csharp/win/build_test_gpu.bat
+++ scripts/tests/csharp/win/build_test_gpu.bat
@@ -4,7 +4,13 @@ set CURRDIR=%CD%
 
 cd ..\..\..\..\libs\projects\SalmonLibs.VS2022
 
+call msbuild -t:restore
+if %ERRORLEVEL% GEQ 1 cd %CURRDIR% && EXIT /B 1
+
 call msbuild /property:Configuration=DebugGPU /p:Platform=x64
+if %ERRORLEVEL% GEQ 1 cd %CURRDIR% && EXIT /B 1
+
+copy .\SalmonNative\x64\DebugGPU\SalmonNative.* .\Salmon.Test\bin\DebugGPU\net8.0-windows\
 if %ERRORLEVEL% GEQ 1 cd %CURRDIR% && EXIT /B 1
 
 cd %CURRDIR%
--- scripts/tests/csharp/win/test_csharp_native.bat
+++ scripts/tests/csharp/win/test_csharp_native.bat
@@ -7,8 +7,6 @@ set ENC_THREADS=1
 
 cd ..\..\..\..\libs\projects\SalmonLibs.VS2022
 
-copy /Y .\x64\Debug\SalmonNative.* Salmon.Test\bin\Debug\net8.0-windows\
-
 set AES_PROVIDER_TYPE=Aes
 call vstest.console Salmon.Test\bin\Debug\net8.0-windows\Salmon.Test.dll /Tests:SalmonNativeTests /Logger:Console;Verbosity=Detailed
 if %ERRORLEVEL% GEQ 1 cd %CURRDIR% && EXIT /B 1
