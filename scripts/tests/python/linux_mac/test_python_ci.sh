b8c690ebb54d7c6cd04f9f7c2c8da338a3c2af15
mku11 <93253421+mku11@users.noreply.github.com>
2025-11-30 18:25:36 +0200

    ci: python

---
+++ scripts/tests/python/linux_mac/test_python_ci.sh
@@ -0,0 +1,11 @@
+CURRDIR=(pwd)
+
+# ALL
+./test_python_core.sh
+./test_python_core_multi.sh
+./test_python_fs.sh
+./test_python_fs_multi.sh
+./test_python_native.sh
+./test_python_native_multi.sh
+
+cd $CURRDIR
