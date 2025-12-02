04f8f63d21f301b34d2e85b19f3c1b9a9d632e53
mku11 <93253421+mku11@users.noreply.github.com>
2025-12-02 17:47:51 +0200

    opencl ignore if dep folder exists

--- scripts/misc/get_opencl.bat
+++ scripts/misc/get_opencl.bat
@@ -4,7 +4,7 @@ set OPENCL_BINARY=OpenCL-SDK-v2025.07.23-Win-x64.zip
 set OPENCL_URL=https://github.com/KhronosGroup/OpenCL-SDK/releases/download/%OPENCL_VERSION%/%OPENCL_BINARY%
 set ZIP_FILENAME=OpenCL.zip
 
-mkdir %DEPS_DIR%
+powershell mkdir -ErrorAction SilentlyContinue %DEPS_DIR%
 curl %OPENCL_URL% -LJo %DEPS_DIR%\%ZIP_FILENAME%
 cd %DEPS_DIR%
 powershell -command Expand-Archive -Force %ZIP_FILENAME%
