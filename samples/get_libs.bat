ec6242a5ac14609fce598cb34dc3feac4a266954
mku11 <93253421+mku11@users.noreply.github.com>
2025-12-03 15:13:10 +0200

    updated samples script

--- samples/get_libs.bat
+++ samples/get_libs.bat
@@ -1,9 +1,9 @@
 set DEPS_DIR=.\libs\
-set SALMON_LIB_VERSION=3.0.2
+set SALMON_LIB_VERSION=3.0.2-SNAPSHOT
 
-set SALMON_BINARY=salmon-multiarch-v%SALMON_VERSION%.zip
+set SALMON_BINARY=salmon-multi-arch.v%SALMON_LIB_VERSION%.zip
 set SALMON_URL=https://github.com/mku11/Salmon-AES-CTR/releases/download/v%SALMON_LIB_VERSION%/%SALMON_BINARY%
-set ZIP_FILENAME=SalmonLibs.zip
+set ZIP_FILENAME=salmon.zip
 
 powershell mkdir -ErrorAction SilentlyContinue %DEPS_DIR%
 curl %SALMON_URL% -LJo %DEPS_DIR%\%ZIP_FILENAME%
--- samples/get_libs.sh
+++ samples/get_libs.sh
@@ -1,9 +1,9 @@
 DEPS_DIR=./libs/
 SALMON_LIB_VERSION=3.0.2
 
-SALMON_BINARY=salmon-multiarch-v%SALMON_VERSION%.zip
-SALMON_URL=https://github.com/mku11/Salmon-AES-CTR/releases/download/v%SALMON_LIB_VERSION%/%SALMON_BINARY%
-ZIP_FILENAME=SalmonLibs.zip
+SALMON_BINARY=salmon-multi-arch.v$SALMON_LIB_VERSION.zip
+SALMON_URL=https://github.com/mku11/Salmon-AES-CTR/releases/download/v$SALMON_LIB_VERSION/$SALMON_BINARY
+ZIP_FILENAME=salmon.zip
 
 mkdir -p $DEPS_DIR
 curl $SALMON_URL -LJo $DEPS_DIR/$ZIP_FILENAME
