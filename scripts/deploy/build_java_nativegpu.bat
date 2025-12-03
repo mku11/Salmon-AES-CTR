e75ae594a851f993849a210d0f9cce9ef19b0069
mku11 <93253421+mku11@users.noreply.github.com>
2025-12-03 13:01:38 +0200

    fixed opencl paths for scripts

--- scripts/deploy/build_java_nativegpu.bat
+++ scripts/deploy/build_java_nativegpu.bat
@@ -1,14 +1,10 @@
 @echo ON
 set CURRDIR=%CD%
 set ENABLE_GPU=true
-REM set SDK_ROOT=D:\\tools\\OpenCL-SDK-v2024.05.08-Win-x64
-set SDK_ROOT=%CURRDIR%\\..\\..\\libs\\deps\\OpenCL\\OpenCL-SDK-v2025.07.23-Win-x64
-set OPENCL_INCLUDE=%SDK_ROOT%\\include
-set OPENCL_LIB=%SDK_ROOT%\\lib
 
 cd ..\..\libs\projects\salmon-libs-gradle
 gradlew.bat --refresh-dependencies & ^
-gradlew.bat build -x test --rerun-tasks -DENABLE_GPU=%ENABLE_GPU% -DOPENCL_INCLUDE=%OPENCL_INCLUDE% -DOPENCL_LIB=%OPENCL_LIB% -i & ^
-gradlew.bat publish --rerun-tasks -DENABLE_GPU=%ENABLE_GPU% -DOPENCL_INCLUDE=%OPENCL_INCLUDE% -DOPENCL_LIB=%OPENCL_LIB% -i & ^
+gradlew.bat build -x test --rerun-tasks -DENABLE_GPU=%ENABLE_GPU% -i & ^
+gradlew.bat publish --rerun-tasks -DENABLE_GPU=%ENABLE_GPU% -i & ^
 package.bat & ^
 cd %CURRDIR%
--- scripts/deploy/build_java_nativegpu.sh
+++ scripts/deploy/build_java_nativegpu.sh
@@ -1,14 +1,12 @@
 CURRDIR=$(pwd)
 
 export ENABLE_GPU=true
-export OPENCL_INCLUDE=/usr/include
-export OPENCL_LIB=/usr/lib/x86_64-linux-gnu/
 
 cd ../../libs/projects/salmon-libs-gradle
 
 ./gradlew --refresh-dependencies
-./gradlew build -x test -i --rerun-tasks -DENABLE_GPU=$ENABLE_GPU -DOPENCL_INCLUDE=$OPENCL_INCLUDE -DOPENCL_LIB=$OPENCL_LIB 
-./gradlew publish --rerun-tasks -DENABLE_GPU=$ENABLE_GPU -DOPENCL_INCLUDE=$OPENCL_INCLUDE -DOPENCL_LIB=$OPENCL_LIB
+./gradlew build -x test -i --rerun-tasks -DENABLE_GPU=$ENABLE_GPU 
+./gradlew publish --rerun-tasks -DENABLE_GPU=$ENABLE_GPU
 ./package.sh
 
 cd $CURRDIR
