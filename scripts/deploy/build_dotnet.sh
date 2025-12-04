c5a49e37076ec0f1a052ce18ef131b076264d737
mku11 <93253421+mku11@users.noreply.github.com>
2025-12-04 12:44:49 +0200

    fixed scripts again

--- scripts/deploy/build_dotnet.sh
+++ scripts/deploy/build_dotnet.sh
@@ -1,8 +1,8 @@
 CURRDIR=$(pwd)
 
 cd ../../libs/projects/SalmonLibs.VS2022
-dotnet workload restore -c Release & ^
-dotnet restore -c Release & ^
-dotnet build --no-restore -c Release & ^
+dotnet workload restore -c Release
+dotnet restore -c Release
+dotnet build --no-restore -c Release
 
 cd $CURRDIR
--- scripts/deploy/build_dotnet_debug.sh
+++ scripts/deploy/build_dotnet_debug.sh
@@ -1,8 +1,8 @@
 CURRDIR=$(pwd)
 
 cd ../../libs/projects/SalmonLibs.VS2022
-dotnet workload restore -c Debug & ^
-dotnet restore -c Debug & ^
-dotnet build --no-restore -c Debug & ^
+dotnet workload restore -c Debug
+dotnet restore -c Debug
+dotnet build --no-restore -c Debug
 
 cd $CURRDIR
