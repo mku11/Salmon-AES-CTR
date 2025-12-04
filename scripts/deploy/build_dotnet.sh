d56fc6f3fc423f49bc5f1965c610d8f87e9833ef
mku11 <93253421+mku11@users.noreply.github.com>
2025-12-04 12:42:36 +0200

    build scripts fixed path

--- scripts/deploy/build_dotnet.sh
+++ scripts/deploy/build_dotnet.sh
@@ -1,6 +1,6 @@
 CURRDIR=$(pwd)
 
-cd ..\..\libs\projects\SalmonLibs.VS2022
+cd ../../libs/projects/SalmonLibs.VS2022
 dotnet workload restore -c Release & ^
 dotnet restore -c Release & ^
 msbuild -c Release & ^
--- scripts/deploy/build_dotnet_debug.sh
+++ scripts/deploy/build_dotnet_debug.sh
@@ -1,6 +1,6 @@
 CURRDIR=$(pwd)
 
-cd ..\..\libs\projects\SalmonLibs.VS2022
+cd ../../libs/projects/SalmonLibs.VS2022
 dotnet workload restore -c Debug & ^
 dotnet restore -c Debug & ^
 msbuild -c Debug & ^
