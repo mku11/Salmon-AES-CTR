f65a351c0b0958eeed23740e361a38a165f05f18
mku11 <93253421+mku11@users.noreply.github.com>
2026-04-26 07:44:23 +0300

    removed version from local webfs server scripts

--- scripts/misc/start_ws_server.bat
+++ scripts/misc/start_ws_server.bat
@@ -2,9 +2,7 @@
 set CURRDIR=%CD%
 set TEST_DIR=c:\tmp\salmon
 
-set WEBFS_VERSION=1.0.0
-
 powershell mkdir -ErrorAction SilentlyContinue %TEST_DIR%\test &^
-cd %CURRDIR%\..\..\libs\deps\WebFS\output\webfs-service\webfs-service-%WEBFS_VERSION% &^
+cd %CURRDIR%\..\..\libs\deps\WebFS\output\webfs-service\webfs-service &^
 start-webfs-service.bat &^
 cd %CURRDIR%
--- scripts/misc/start_ws_server.sh
+++ scripts/misc/start_ws_server.sh
@@ -1,8 +1,6 @@
 CURRDIR=$(pwd)
 
-WEBFS_VERSION=1.0.0
-
 mkdir -p /tmp/salmon
-cd ../../libs/deps/WebFS/output/webfs-service/webfs-service-$WEBFS_VERSION
+cd ../../libs/deps/WebFS/output/webfs-service/webfs-service
 ./start-webfs-service.sh
 cd $CURRDIR
