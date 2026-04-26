bd10686ea96fbea8150c8c3f5d7b99298e55c5ca
mku11 <93253421+mku11@users.noreply.github.com>
2026-04-26 07:38:31 +0300

    removed version from webfs local artifact for easier testing

--- scripts/misc/setup_webfs.bat
+++ scripts/misc/setup_webfs.bat
@@ -5,6 +5,6 @@ cd ..\..\libs\deps\WebFS\project &^
 gradlew.bat bootWar &^
 cd webfs-service &^
 call package.bat &^
-cd ..\..\output\webfs-service\webfs-service-1.0.0\config &^
+cd ..\..\output\webfs-service\webfs-service\config &^
 copy /Y ..\..\..\..\..\..\test\config\application.properties.win.test application.properties &^
 cd %CURRDIR%
--- scripts/misc/setup_webfs.sh
+++ scripts/misc/setup_webfs.sh
@@ -4,7 +4,7 @@ cd ../../libs/deps/WebFS/project
 ./gradlew bootWar
 cd webfs-service
 ./package.sh
-cd ../../output/webfs-service/webfs-service-1.0.0/config
+cd ../../output/webfs-service/webfs-service/config
 cp -f ../../../../../../test/config/application.properties.linux.macos.test application.properties
 
 cd $CURRDIR
